98511a0171264ace3d3e78a74a3fdb45d625e5bb
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-17 20:51:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def long_planeteer_calls(array) # code an argument here
 end
 
 def find_the_cheese(array) # code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end # the array below is here to help
 end
