5fc5dba380aa8c8ad4aa74cf19b9336258b7d73b
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-17 20:49:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array) # code an argument here
-  array.# Your code here
+  array.any? do |# Your code here
 end
 
 def find_the_cheese# code an argument here
